c2f0cf98704e29eff67f0f43c2b621deec4727ea
rajy4683 <st.hazard@gmail.com>
2021-06-11 00:34:24 +0530

    Updated plotting code

--- miniRekog/utils/fileutils.py
+++ miniRekog/utils/fileutils.py
@@ -10,6 +10,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import torch
 import torchvision
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 def show_sample_images(images, labels, classes, max_count=25):
@@ -104,3 +107,73 @@ def split_df(df_data_list, split_pct=0.7):
     df_test = df_data_list.loc[test_set].reset_index()
     df_test.drop('index',axis=1,inplace=True)
     return df_train, df_test
+
+#### Function to plot misclassified images from dataset loader
+def plot_misclassified(args, model, device, test_loader,classes,epoch_number):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    preds = np.array([])
+    actuals = np.array([])
+    error_images = []
+    total_misclassified = 0
+    total_rounds = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            #print(len(data))
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            #torch.
+            #np.append(preds, pred.squeeze().cpu())
+            orig_labels = target.cpu().numpy()
+            pred_labels = pred.squeeze().cpu().numpy()
+            #print(orig_labels.shape, pred_labels.shape)
+            mislabeled_index = np.where(orig_labels != pred_labels)[0]
+            #print(orig_labels)
+            #print(pred_labels)
+            total_rounds+=1
+            if (mislabeled_index.shape[0] > 0):
+                #print(mislabeled_index)
+                for iCount in range(len(mislabeled_index)):
+                    #print(transforms.Normalize()data[offset]((-0.1307,), (1/0.3081,)))
+                    #plt.imshow(data[offset].cpu().numpy().squeeze(), cmap='gray_r')
+                    offset = mislabeled_index[iCount]
+                    error_images.append(data[offset].cpu().numpy().squeeze())
+                    preds=np.append(preds, pred_labels[offset])
+                    actuals = np.append(actuals, orig_labels[offset])
+                    total_misclassified += 1
+                #error_images.append(data[mislabeled_index].cpu().numpy())#,axis=1)
+                #preds=np.append(preds, pred_labels[mislabeled_index])
+
+
+        #example_images.append(wandb.Image(
+        #        data[0], caption="Pred: {} Truth: {}".format(classes[pred[0].item()], classes[target[0]])))
+    #print("Total images worked on:",total_rounds)
+    test_loss /= len(test_loader.dataset)
+    test_accuracy = (100. * correct) / len(test_loader.dataset)
+    print((total_misclassified))
+    print(preds.shape)
+    #print('\nEpoch: {:.0f} Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.3f}%)\n'.format(
+    #    epoch_number, test_loss, correct, len(test_loader.dataset),
+    #    100. * correct / len(test_loader.dataset)))
+    #test_acc.append(test_accuracy)
+    #test_losses.append(test_loss)
+
+    # figure = plt.figure()
+    # num_of_images = total_misclassified
+    # for index in range(1, num_of_images):
+    #     plt.subplot(5, 13, index)
+    #     plt.axis('off')
+    #     plt.imshow(error_images[index], cmap='gray_r')
+    #     #plt.label
+    fig = plt.figure(figsize=(10,10))
+    for idx in np.arange(25):
+        ax = fig.add_subplot(5, 5, idx+1, xticks=[], yticks=[])
+        plt.imshow(error_images[idx], cmap='gray_r')
+        #ax.set_title("Pred="+str(np.int(preds[idx])))
+        ax.set(ylabel="Pred="+str(np.int(preds[idx])), xlabel="Actual="+str(np.int(actuals[idx])))
+
+    return test_accuracy, test_loss, None#error_images#, data(np.where(orig_labels != pred_labels))
