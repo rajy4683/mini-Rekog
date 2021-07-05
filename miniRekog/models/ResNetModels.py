13f9689a7d2353194f3303b788b5669153f75741
rajy4683 <st.hazard@gmail.com>
2021-07-05 22:15:36 +0530

    Custom ResNet models

--- miniRekog/models/ResNetModels.py
+++ miniRekog/models/ResNetModels.py
@@ -136,3 +136,110 @@ def ResNetLN34():
         Returns the Resnet34 model
     """
     return ResNet(BasicLNBlock, [2, 2, 2, 2], norm_type="LN")
+
+
+
+class ModifiedResBlock(nn.Module):
+    """
+        ModifiedResBlock: Class for creating Modified ResNet block. Based on S11:
+        X = Conv 3x3 (s1, p1) >> MaxPool2D >> BN >> RELU
+        R1 = ResBlock( (Conv-BN-ReLU-Conv-BN-ReLU))(X) 
+        Add(X, R1)
+
+    """
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(ModifiedResBlock, self).__init__()
+        self.layerconv = nn.Sequential(
+            nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(),
+        )
+        ### This layer applies after the first conv and we intend to keep the channel size same
+        self.resconv = nn.Sequential(
+            nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(),
+            nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(planes),
+            nn.ReLU()
+            )
+        #self.shortcut = nn.Sequential() 
+
+    def forward(self, x):
+        out = self.layerconv(x)
+        res = self.resconv(out)
+        #out = res
+        #out = F.relu(out)
+        return out+res
+
+
+
+class S8ResNet(nn.Module):
+    """
+        S8: Custom resnet block based model
+        It used the ModifiedResBlock which doesnt have multiple layers.
+        PrepLayer:
+            Conv 3x3 s1, p1) >> BN >> RELU [64]
+        Layer1:
+            ModifiedResBlock(128)
+        Layer 2:
+            Conv 3x3 [256]
+            MaxPooling2D
+            BN
+            ReLU
+        Layer 3:
+            ModifiedResBlock(512)
+        MaxPooling:(with Kernel Size 4) 
+        FC Layer 
+        SoftMax
+    """
+    def __init__(self, num_classes=10,dropout=0.0):
+        super(S8ResNet, self).__init__()
+        self.in_planes = 64
+        self.resize_layer = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+
+        self.prep_layer = nn.Sequential(
+            nn.Conv2d(64, self.in_planes, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(self.in_planes),
+            nn.ReLU()
+        )
+        self.layer1 = ModifiedResBlock(self.in_planes, self.in_planes*2, 1)
+            
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(self.in_planes*2, self.in_planes*4, 
+                    kernel_size=3, 
+                    stride=1, 
+                    padding=1, 
+                    bias=False),
+            nn.MaxPool2d(2,2),
+            nn.BatchNorm2d(self.in_planes*4),
+            nn.ReLU()
+        )
+        self.layer3 = ModifiedResBlock(self.in_planes*4, self.in_planes*8, 1)
+        self.layer4_supermax = nn.MaxPool2d(4,4)
+        self.fc_layer = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        out = self.resize_layer(x)
+        out = self.prep_layer(out)
+        #out = self.prep_layer(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4_supermax(out)
+        out = out.view(-1, 512)
+        #out = out.view(-1, 10)
+        out = self.fc_layer(out)
+        #
+        out = F.log_softmax(out, dim=1)
+        return out
